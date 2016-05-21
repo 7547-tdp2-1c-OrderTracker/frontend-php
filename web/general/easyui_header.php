9d91b8bbd0c48c3abd813688d27faf594dff7e51
Dario Seminara <darios3@gmail.com>
2016-05-21 12:53:36 -0300

    Revert "descargado css de easyui"
    This reverts commit f4c236e28c20ea672d4ba063d317b75228182255.

--- web/general/easyui_header.php
+++ web/general/easyui_header.php
@@ -1,6 +1,6 @@
-<link rel="stylesheet" type="text/css" href="easyui/easyui.css">
-<link rel="stylesheet" type="text/css" href="easyui/icon.css">
-<link rel="stylesheet" type="text/css" href="easyui/demo.css">
+<link rel="stylesheet" type="text/css" href="http://www.jeasyui.com/easyui/themes/default/easyui.css">
+<link rel="stylesheet" type="text/css" href="http://www.jeasyui.com/easyui/themes/icon.css">
+<link rel="stylesheet" type="text/css" href="http://www.jeasyui.com/easyui/demo/demo.css">
 <script type="text/javascript" src="lib/jquery.min.js"></script>
 <script type="text/javascript" src="lib/jquery.easyui.min.js"></script>
 <script type="text/javascript" src="lib/easyui-lang-es.js"></script>
