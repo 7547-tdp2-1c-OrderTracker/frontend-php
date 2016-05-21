8f6844de5fa0a069c4ea9f46ce5ff42b1a9269ac
Dario Seminara <darios3@gmail.com>
2016-05-21 12:48:34 -0300

    separado formatter de header (para poder debuggearlo)

--- web/general/easyui_header.php
+++ web/general/easyui_header.php
@@ -4,25 +4,4 @@
 <script type="text/javascript" src="lib/jquery.min.js"></script>
 <script type="text/javascript" src="lib/jquery.easyui.min.js"></script>
 <script type="text/javascript" src="lib/easyui-lang-es.js"></script>
-<script>
-	$.extend($.fn.datebox.defaults,{  // formato de fecha DD/MM/YYYY
-		formatter:function(date){
-			var y = date.getFullYear();
-			var m = date.getMonth()+1;
-			var d = date.getDate();
-			return (d<10?('0'+d):d)+'/'+(m<10?('0'+m):m)+'/'+y;
-		},
-		parser:function(s){
-			if (!s) return new Date();
-			var ss = s.split('/');
-			var d = parseInt(ss[0],10);
-			var m = parseInt(ss[1],10);
-			var y = parseInt(ss[2],10);
-			if (!isNaN(y) && !isNaN(m) && !isNaN(d)){
-				return new Date(y,m-1,d);
-			} else {
-				return new Date();
-			}
-		}
-	});
-</script>
+<script type="text/javascript" src="general/date-formatter.js"></script>
