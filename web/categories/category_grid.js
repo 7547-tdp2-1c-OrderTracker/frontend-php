1b86316911df5c73ca59ef7b898a60e2feffbed2
Dario Seminara <darios3@gmail.com>
2016-05-25 22:04:50 -0300

    ABM Categorias

---
+++ web/categories/category_grid.js
@@ -0,0 +1,90 @@
+$("#dg").datagrid({
+  loader: function(params, success, error) {
+    $.ajax({
+      url: window.apiBaseUrl + "/v1/categories?page=" + params.page + "&rows=" + params.rows,
+      method: "GET",
+      success: success,
+      error: error,
+      headers: {
+        authorization: Cookies.get("tmtoken")
+      }
+    });
+  }
+});
+
+var url ;
+var method ;
+
+function editCategory() {
+  var row = $('#dg').datagrid('getSelected');
+  if(row) {
+    url = window.apiBaseUrl + "/v1/categories/" + row.id;
+    method = "PUT";
+
+    $('#dlg').dialog('open').dialog('setTitle','Editar Categoria');
+    $('#fm').form('load',row);
+  }
+}
+
+function newCategory() {
+  url = window.apiBaseUrl + "/v1/categories";
+  method = "POST";
+
+  $('#dlg').dialog('open').dialog('setTitle','Nueva Categoria');
+  $('#fm').form('clear');
+}
+
+function saveCategory() {
+  $.ajax({
+    url: url, 
+    method: method, 
+    data: {
+      name: $(".category-form input[name='name']").val(),
+      description: $(".category-form input[name='description']").val()
+    },
+    headers: {
+      authorization: Cookies.get("tmtoken")
+    },
+    success: function(result) {
+      if(result.error) {
+        $.messager.show({
+          title: 'Error',
+          msg: result.error.value
+        });
+      } else {
+        $('#dlg').dialog('close');    // close the dialog
+        $('#dg').datagrid('reload');  // reload the user data
+      }
+    }
+  });
+}
+
+function deleteCategory() {
+  var row = $('#dg').datagrid('getSelected');
+  if(row) {
+    $.ajax({
+      url: window.apiBaseUrl + "/v1/categories/" + row.id,
+      method: 'DELETE',
+      headers: {
+        authorization: Cookies.get("tmtoken")
+      },
+      success: function(result) {
+        if(result.error) {
+          $.messager.show({
+            title: 'Error',
+            msg: result.error.value
+          });
+        } else {
+          $('#dlg').dialog('close');    // close the dialog
+          $('#dg').datagrid('reload');  // reload the user data
+        }
+      }
+    });
+  }
+}
+
+$('#dg').datagrid({
+  onLoadSuccess: function(data){
+    gridResize();
+  }
+});
